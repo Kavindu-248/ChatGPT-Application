e8b018d9472a20669e68bc564a2461c2c81c7cbb
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-04 16:22:50 +0530

    server folder

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/bog.svg';
+import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
@@ -37,5 +37,63 @@ function typeText(element, text){
 }
 
 function generateUniqueId(){
-  
+
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+
+  return `id-${timestamp}-${hexadecimalString}`;
+
 }
+
+function chatStripe (isAi, value, uniqueId) {
+  return(
+    `
+     <div class = "wrapper ${isAi && 'ai'}">
+      <div class = "chat">
+        <div className = "profile">
+          <img
+            src = "${isAi ? bot : user}"
+            alt = "${isAi ? 'bot' : 'user'}"
+
+          />
+          </div>
+         <div class = "message" id = ${uniqueId }>${value}</div>
+       </div>
+     </div>  
+    `
+  )
+
+}
+
+const handleSubmit = async (e) =>{
+  e.preventDefault();
+
+  const date = new FormData(form);
+
+  //user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  //bot'chatstripe
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+}
+
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup' , (e) => {
+  if(e.keyCode === 13){
+    handleSubmit(e);
+  }
+})
